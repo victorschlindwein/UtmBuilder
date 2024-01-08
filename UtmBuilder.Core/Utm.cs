92dd07ee7e10f5001e8312def147ab122f326c3e
dizzysch <victorwilbert@gmail.com>
2024-01-08 16:27:37 -0300

    Adicionar arquivos de projeto.

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core
+{
+    public class Utm 
+    {
+        public Url Url { get; set; }
+        public string Source { get; set; }
+        public string Medium { get; set; }
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Term { get; set; }
+        public string Content { get; set; }
+    }
+}
