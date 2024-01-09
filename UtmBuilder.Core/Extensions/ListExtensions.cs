d7069b5e324d002f5cc708e807e169548a0b9c54
dizzysch <victorwilbert@gmail.com>
2024-01-09 11:32:25 -0300

    feat: add extansion methods and implicit operators

--- UtmBuilder.Core/Extensions/ListExtensions.cs
+++ UtmBuilder.Core/Extensions/ListExtensions.cs
@@ -2,6 +2,13 @@
 {
     public static class ListExtensions
     {
-        public static void AddIfNotNull(this List<string> list) { }
+        public static void AddIfNotNull(
+            this List<string> list,
+            string key,
+            string? value)
+        {
+            if(!string.IsNullOrEmpty(value))
+                list.Add($"{key}={value}");
+        }
     }
 }
