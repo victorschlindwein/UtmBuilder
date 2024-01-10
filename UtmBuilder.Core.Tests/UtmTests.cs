77b9f48fed1b8dcdfcdc6f6fce232c6f6e8d53e8
dizzysch <victorwilbert@gmail.com>
2024-01-10 17:50:30 -0300

    tests: add tests

---
+++ UtmBuilder.Core.Tests/UtmTests.cs
@@ -0,0 +1,48 @@
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core.Tests
+{
+    [TestClass]
+    public class UtmTests
+    {
+        private const string Result = "https://victorschlindwein.com/" +
+                                      "?utm_source=source" +
+                                      "&utm_medium=medium" +
+                                      "&utm_campaign=name" +
+                                      "&utm_id=id" +
+                                      "&utm_term=term" +
+                                      "&utm_content=content";
+
+        private readonly Url _url = new("https://victorschlindwein.com/");
+        private readonly Campaign _campaign = new(
+            "source",
+            "medium",
+            "name",
+            "id",
+            "term",
+            "content"
+        );
+
+        [TestMethod]
+        public void ShouldReturnUrlFromUtm()
+        {
+            var utm = new Utm(_url, _campaign);
+            
+            Assert.AreEqual(Result, utm.ToString());
+            Assert.AreEqual(Result, (string)utm);
+        }
+
+        [TestMethod]
+        public void ShouldReturnUtmFromUrl()
+        {
+            Utm utm = Result;
+            Assert.AreEqual("https://victorschlindwein.com/", utm.Url.Address);
+            Assert.AreEqual("source", utm.Campaign.Source);
+            Assert.AreEqual("medium", utm.Campaign.Medium);
+            Assert.AreEqual("name", utm.Campaign.Name);
+            Assert.AreEqual("id", utm.Campaign.Id);
+            Assert.AreEqual("term", utm.Campaign.Term);
+            Assert.AreEqual("content", utm.Campaign.Content);
+        }
+    }
+}
---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,54 @@
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class UrlTests
+    {
+        private const string InvalidUrl = "jakiro";
+        private const string ValidUrl = "https://victorschlindwein.com";
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUrlException))]
+        public void ShoudReturnExceptionWhenInvalidUrl()
+        {
+            var url = new Url(InvalidUrl);
+        }
+
+        [TestMethod]
+        public void ShoudNotReturnExceptionWhenInvalidUrl()
+        {
+            new Url(ValidUrl);
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        [DataRow(" ", true)]
+        [DataRow("http", true)]
+        [DataRow(InvalidUrl, true)]
+        [DataRow(ValidUrl, false)]
+        public void TestUrl(
+            string link,
+            bool expectException)
+        {
+            if (expectException)
+            {
+                try
+                {
+                    new Url(link);
+                    Assert.Fail();
+                }
+                catch (InvalidUrlException)
+                {
+                    Assert.IsTrue(true);
+                }
+            }
+            else
+            {
+                new Url(link);
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
